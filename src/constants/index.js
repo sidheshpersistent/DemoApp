6f05e7ace4de516d56852313d303fced908c19be
sidheshpersistent <sidhesh_manchekar@persistent.com>
2022-02-17 12:25:33 +0530

    16th_feb/7:28PM

---
+++ src/constants/index.js
@@ -0,0 +1,39 @@
+
+/* import iconConstants from './icon.constants'; */
+/* import { spannbleConstant, paymentSuccessText, childCountText, spannableIterate } from './spannbleConstant'; */
+
+/* import dropDownConstants from './dropDownConstants'; */
+/* import {screensConst} from './stack.constants'; */
+/* import { productSelectionConstant } from './productSelectionConstant'; */
+import {keyboardTypes, keyboardConst} from './keyboard.constant';
+
+import deviceConstants from './deviceConstants';
+
+import {
+  COMMON_CONST,
+  PERSON_DETAILS,
+  DBTvaluesArr,
+  CONSTANTS,
+  FORM_FIELD_CONST,
+  CARDTYPE,
+} from './constants';
+
+export {
+    keyboardTypes,
+    PERSON_DETAILS,
+   // screensConst,
+    //spannbleConstant,
+   // iconConstants,
+    COMMON_CONST,
+   // paymentSuccessText,
+   // childCountText,
+    //productSelectionConstant,
+    DBTvaluesArr,
+   // dropDownConstants,
+    CONSTANTS,
+    deviceConstants,
+    FORM_FIELD_CONST,
+   // spannableIterate,
+    keyboardConst,
+    CARDTYPE,
+  };
