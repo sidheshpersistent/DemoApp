0228f615d7faf5a04665f0158d232894f3238074
subratkumarbal5 <subratkumarbal5@gmail.com>
2022-02-21 18:06:36 +0530

    login screen demo

--- src/app/AppContainer.js
+++ src/app/AppContainer.js
@@ -3,6 +3,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import Dashboard from '../Screens/Dashboard/Dashboard';
 import ModelTestScreen from '../Screens/ModalTestScreen.js/ModalTestScreen';
+import LoginScreen from '../Screens/LogIn/LoginScreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -11,8 +12,8 @@ const AppContainer = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-       
        screenOptions={{ headerShown: false}} >
+        <Stack.Screen name={'LoginScreen'} component={LoginScreen} />
         <Stack.Screen name={'Dashboard'} component={Dashboard} />
         <Stack.Screen name={'ModelTestScreen'} component={ModelTestScreen} />
       </Stack.Navigator>
