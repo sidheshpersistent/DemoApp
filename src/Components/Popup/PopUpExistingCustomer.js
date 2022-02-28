22068c2ddbc8854eadb946c87a4db1389f24b418
subratkumarbal5 <subratkumarbal5@gmail.com>
2022-02-28 20:21:08 +0530

    chnages

--- src/Components/Popup/PopUpExistingCustomer.js
+++ src/Components/Popup/PopUpExistingCustomer.js
@@ -15,8 +15,17 @@ import {
   PopupHeading,
   SubTextContainer,
   TopIconView,
+  AccountDetailsCardContainer,
+  AccountDetailsColumn,
+  AccountTypeText,
+  AccountNumberText,
+  AccountListView,
+  ChooseReasonView,
+  AccountOpeningMsg,
+  ReasonText
 } from './PopupStyle';
 import CustomBlurView from './CustomBlurView';
+import AutoCompleteTextInput from '../../components/AutoCompleteTextInput/AutoCompleteTextInput';
 
 const PopUpExistingCustomer = props => {
   const {
@@ -45,22 +54,52 @@ const PopUpExistingCustomer = props => {
             <View style={{paddingLeft: 24, paddingRight: 24}}>
               
               <PopupHeading>{heading}</PopupHeading>
-              <SubTextContainer><Text>{subText}</Text></SubTextContainer>
-              <View style={{backgroundColor:'#ffffff',width:'100%',paddingVertical:20, marginTop:10, borderRadius:10}}>
+               <SubTextContainer><Text>{subText}</Text></SubTextContainer>
+              <AccountListView>
               {
                 data?.accountList?.map((account)=>{
                   /**TODO: added a key to avoid similar key issue */
                    return <AccountDetailsCard key={account.accountType} account={account}/>
                 })
               }
+              </AccountListView>
+              <ChooseReasonView>
+                <AccountOpeningMsg>
+                  <Text>{`Customer’s registered mobile number`}</Text>
+                  <Text style={{fontWeight:'700'}}>{` +91 70857 62345`}</Text>
+                  <Text>{` will be used for account opening`}</Text>
+                </AccountOpeningMsg>
+              </ChooseReasonView>
+              <ReasonText>{`Select reason for opening new account`}</ReasonText>
+              <View>
+              <AutoCompleteTextInput
+                      testID={'12345'}
+                      name={`Reason`}
+                      invalid={false}
+                      maxLength={40}
+                      isRightImage={true}
+                      rightImage={require('../../assets/icons_24_chevron_down.png')}
+                      // errorMessage={errors?.cityBal?.message}
+                      // data={businessCities}
+                      value={''}
+                      onChangeText={text => {
+                        //   onChange(text);
+                      }}
+                      placeholder={`Reason`}
+                      // onSelectListItem={item => onSelectCity(item, onChange)}
+                    />
               </View>
-              
             </View>
             {/** TODO: this button may required to delete after getting ccl library */}
             <TouchableOpacity
               onPress={() => buttonPress()}
               style={styles.Button}>
               <Text style={styles.ButtonText}>{ButtonText}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => buttonPress()}
+              style={styles.cancelBtn}>
+              <Text style={styles.cancelBtnTxt}>{`Cancel`}</Text>
             </TouchableOpacity>
           </ModalView>
         </CenteredView>
@@ -72,28 +111,50 @@ const PopUpExistingCustomer = props => {
 const AccountDetailsCard = (account)=>{
   console.log(account);
   return(
-    <View  style={{ flexDirection:'row'}}>
+    <AccountDetailsCardContainer>
 
-      <View style={{flex:1, paddingBottom:5,paddingTop:10,paddingLeft:10,paddingRight:10}}>
-        <Text style={{color:'#686873', fontSize:10, lineHeight:14, letterSpacing:0.5}}>{`ACCOUNT`}</Text>
-        <Text style={{color:'black', fontSize:14, lineHeight:20, letterSpacing:-0.5}}>{account?.account?.accountType}</Text>
-      </View>
+      <AccountDetailsColumn>
+        <AccountTypeText>{`ACCOUNT`}</AccountTypeText>
+        <AccountNumberText>{account?.account?.accountType}</AccountNumberText>
+      </AccountDetailsColumn>
 
-      <View style={{flex:1,  paddingBottom:5,paddingTop:10,paddingLeft:10,paddingRight:10}}>
-        <Text style={{color:'#686873', fontSize:10, lineHeight:14, letterSpacing:0.5}}>{`ACCOUNT`}</Text>
-        <Text style={{color:'black', fontSize:14, lineHeight:20, letterSpacing:-0.5}}>{account?.account?.accountNumber}</Text>
-      </View>
+      <AccountDetailsColumn>
+        <AccountTypeText>{`ACCOUNT`}</AccountTypeText>
+        <AccountNumberText>{account?.account?.accountNumber}</AccountNumberText>
+      </AccountDetailsColumn>
        
-    </View>
+    </AccountDetailsCardContainer>
    
   )
 }
 // TODO: will be replaced with ccl button
 const styles = StyleSheet.create({
+  cancelBtn:{
+    alignSelf: 'center',
+    marginTop: 0,
+    marginBottom: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 200,
+    height: 56,
+  },
+  cancelBtnTxt: {
+    fontWeight: '600',
+    width: 174,
+    height: 24,
+    fontFamily: 'Inter',
+    fontSize: 17,
+    alignSelf: 'center',
+    fontStyle: 'normal',
+    lineHeight: 24,
+    letterSpacing: -0.6,
+    textAlign: 'center',
+    color: '#9b1e26',
+  },
   Button: {
     alignSelf: 'center',
     marginTop: 20,
-    marginBottom: 40,
+    marginBottom: 20,
     alignItems: 'center',
     justifyContent: 'center',
     width: 200,
--- src/Components/Popup/PopupStyle.js
+++ src/Components/Popup/PopupStyle.js
@@ -10,7 +10,7 @@ export const ModalView = styled.View`
  
   justify-content:center;
  
-  width: 90%;  /**TODO: changed from 70 to 90 as there was conflict with UI on Screen, Have to be discussed  */
+  width: 70%;  /**TODO: changed from 70 to 90 as there was conflict with UI on Screen, Have to be discussed  */
   background-color: #f6f6f6;
   border-radius: 16px;
   shadow-color: black;
@@ -30,10 +30,6 @@ export const PopupHeading = styled.Text`
 
 export const SubTextContainer = styled.View`
   margin-top: 20px;  
- 
-  
-  
-
 `;
 
 export const TopIconView = styled.View`
@@ -44,4 +40,61 @@ export const TopIconView = styled.View`
   align-self: center;
   justify-content: center;
   align-items: center;
+`;
+
+export const AccountDetailsCardContainer = styled.View`
+  flexDirection:row;
+`;
+
+export const AccountDetailsColumn = styled.View`
+  flex:1; 
+  padding-bottom:5px;
+  padding-top:10px;
+  padding-left:20px;
+  padding-right:20px;
+`;
+
+export const AccountTypeText = styled.Text`
+  color:#686873;
+  font-size:10px;
+  line-height:14px;
+  letter-spacing:0.5px;
+`;
+
+export const AccountNumberText = styled.Text`
+  color:#000000;
+  font-size:14px;
+  line-height:20px;
+  letter-spacing:-0.5px;
+`;
+
+export const AccountListView = styled.View`
+  background-color:#ffffff;
+  width:100%;
+  padding-vertical:20px;
+  margin-top:10px;
+  border-radius:10px;
+`;
+
+export const ChooseReasonView = styled.View`
+  background-color:#25243b;
+  border-radius:10px;
+  margin-top:16px;
+`;
+
+export const AccountOpeningMsg = styled.Text`
+  color:#ffffff;
+  font-size:12px;
+  line-height:18px;
+  letter-spacing:-0.2px;
+  padding-horizontal:20px;
+  padding-vertical:16px;
+`;
+
+export const ReasonText = styled.Text`
+  color:#25243b;
+  padding-vertical:20px;
+  font-size:14px;
+  line-height:20px;
+  letter-spacing:-0.5px;
 `;
