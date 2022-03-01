29d3076b607cbf29626f213eed8ba882972d35ed
Ayush Mishra <74084740+rnqayush@users.noreply.github.com>
2022-03-01 10:55:54 +0530

    Update PopupStyle.js

--- src/Components/Popup/PopupStyle.js
+++ src/Components/Popup/PopupStyle.js
@@ -10,7 +10,7 @@ export const ModalView = styled.View`
  
   justify-content:center;
  
-  width: 70%;  /**TODO: changed from 70 to 90 as there was conflict with UI on Screen, Have to be discussed  */
+  width: 80%;  /**TODO: changed from 70 to 90 as there was conflict with UI on Screen, Have to be discussed  */
   background-color: #f6f6f6;
   border-radius: 16px;
   shadow-color: black;
