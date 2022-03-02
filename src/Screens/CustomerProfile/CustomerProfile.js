d5a22b6c406be5db0d6fa1008f9df0cb39224d6c
sidheshpersistent <sidhesh_manchekar@persistent.com>
2022-03-02 10:06:15 +0530

    customer profile added in development

--- src/Screens/CustomerProfile/CustomerProfile.js
+++ src/Screens/CustomerProfile/CustomerProfile.js
@@ -1,14 +1,6 @@
 import React, {useEffect, useState} from 'react';
 
-import {
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  
-  
-  Dimensions,
-} from 'react-native';
+import {View, Text, Image, TouchableOpacity, Dimensions} from 'react-native';
 import styled from 'styled-components/native';
 import BackgroundImage from '../../components/BackgroundImage/BackgroundImage';
 import UpperBoxContainer from '../../components/UpperBoxContainer/UpperBoxContainer';
@@ -17,51 +9,49 @@ import ProfileHeaderContainer from 'components/ProfileHeaderContainer';
 const CustomerProfile = () => {
   return (
     <BackgroundImage>
-
       <HeaderContainer>
         <CloseAndSave>
-        <Text>ad</Text>
-        <SaveAndExit>Save & Exit</SaveAndExit>
+          <Text>ad</Text>
+          <SaveAndExit>Save & Exit</SaveAndExit>
         </CloseAndSave>
 
         <ProfileHeaderContainer
           style={header}
           maxContainerHeight={200}
           leftView={
-            <View >
+            <View>
               <Image
                 style={image}
                 source={require('../Dashboard/testImg.jpg')}
               />
             </View>
           }
-          rightView={<View style={{justifyContent:"center",alignItems:"center"}}>
-              <Text >Astha Patil</Text>
-              <Text >28 | Female</Text>
-          </View>}
+          rightView={
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+              <Text>Astha Patil</Text>
+              <Text>28 | Female</Text>
+            </View>
+          }
         />
       </HeaderContainer>
 
       <PleaseEnter>Please enter the customer’s personal details</PleaseEnter>
-    
-          <Conatainer>
 
-          </Conatainer>
-          
+      <Conatainer></Conatainer>
     </BackgroundImage>
   );
 };
 
 export default CustomerProfile;
 
-const  HeaderContainer = styled(UpperBoxContainer)`
+const HeaderContainer = styled(UpperBoxContainer)`
   height: 158px;
   padding: 12px 15px 26px 12px;
-  margin-bottom:38px
+  margin-bottom: 38px;
 `;
-const  CloseAndSave = styled.View`
- flex-direction:row;
- justify-content:space-between
+const CloseAndSave = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
 const SaveAndExit = styled.Text`
@@ -74,14 +64,13 @@ const SaveAndExit = styled.Text`
   letter-spacing: -0.5px;
   text-align: right;
   color: #9b1e26;
-`
-const Conatainer=styled.View`
-    background-color:white;
-    flex:1
-
-`
+`;
+const Conatainer = styled.View`
+  background-color: white;
+  flex: 1;
+`;
 const PleaseEnter = styled.Text`
-margin-bottom:40px;
+  margin-bottom: 40px;
   width: 406px;
   height: 26px;
   font-family: Inter;
@@ -91,8 +80,8 @@ margin-bottom:40px;
   letter-spacing: -0.5px;
   text-align: center;
   color: #ffffff;
-`
-const header = {elevation: 0, padding: 0, marginTop: 15,alignItems:"center"}
+`;
+const header = {elevation: 0, padding: 0, marginTop: 15, alignItems: 'center'};
 const image = {
   width: 48,
   height: 48,
