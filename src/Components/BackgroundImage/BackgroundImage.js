3e72e0e7a90c61a241a64be6181e58be4e0f0549
NeeleshAtalePersistent <neelesh_atale@persistent.com>
2022-12-15 12:08:40 +0530

    test

--- src/Components/BackgroundImage/BackgroundImage.js
+++ src/Components/BackgroundImage/BackgroundImage.js
@@ -3,6 +3,9 @@ import { ImageBackground } from 'react-native';
 import BGImage from '../../Assets/Images/bg1.png';
 const BackgroundImage = props => {
   const { style, imageSource, imageStyle } = props;
+
+
+  
   return (
     <ImageBackground
       imageStyle={imageStyle}
